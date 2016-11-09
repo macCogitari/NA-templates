cc40a87b08f6a50fd241a9b4e61ddb68b48eee3e
Maciej Trojnar <maciej.trojnar.ext@nokia.com>
2016-11-09 16:04:19 +0100

    container_wrapper actual first implementation

--- 20-type-template/implementation.h
+++ 20-type-template/implementation.h
@@ -1,5 +1,27 @@
 #pragma once
 
+#include <cstddef>
+#include <utility>
+
 template<typename T>
-class container_wrapper;
+class container_wrapper{
+//private:
+//    T* _container;
+public:
+//    container_wrapper():_container{nullptr}{}
+//    container_wrapper(T && container):_container{container}{}s
+    container_wrapper() = default;
+    container_wrapper(T container):_container(std::move(container)){}
+
+    container_wrapper(const container_wrapper &) = default; // to samo co container_wrapper(const container_wrapper<T> &)
+     // nie opuszczać tego const na górze.
+    container_wrapper(container_wrapper &&) = default;
+
+    container_wrapper & operator = (const container_wrapper &) = default;
+    container_wrapper & operator = (container_wrapper &&) = default;
+
+    std::size_t size() const{return _container.size();}
+private:
+    T _container;
+};
 
