45110e04af4b772957baf48c25a56d0bb5a3b850
Maciej Trojnar <maciej.trojnar.ext@nokia.com>
2016-11-10 12:49:35 +0100

    magic c++11 style refactorization of implementation.h

--- 30-traits/implementation.h
+++ 30-traits/implementation.h
@@ -5,10 +5,8 @@
 
 
 template<typename T, typename T1, typename T2, typename T3>
-struct is_any_of : std::bool_constant<
-        std::is_same<T, T1>::value
-        || std::is_same<T, T2>::value
-        || std::is_same<T, T3>::value
+struct is_any_of : std::disjunction<
+        std::is_same<T, T1>,  std::is_same<T, T2>,  std::is_same<T, T3>
         >
 {
 
