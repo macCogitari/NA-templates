ed739e695b3ef9e6485355f4e2ad0d91a0015706
Maciej Trojnar <maciej.trojnar.ext@nokia.com>
2016-11-10 16:01:16 +0100

    implementation of vector_erase.h

--- 40-algos-impl/vector_erase.h
+++ 40-algos-impl/vector_erase.h
@@ -1,4 +1,14 @@
 #pragma once
 
-// define vector_erase here
+#include "algorithm"
 
+// define vector_erase here
+//template<typename T, typename Alloc>
+//void vector_erase(std::vector<T, Alloc> & vec, const T& elem ){
+//    vec.erase(std::remove(vec.begin(), vec.end(), elem), vec.end());
+//}
+
+template<typename T>
+void vector_erase(T & vec, const typename T::value_type& elem ){
+    vec.erase(std::remove(vec.begin(), vec.end(), elem), vec.end());
+}
