7d7e0997a68873d779a7886e96d0f1791f2088b4
Maciej Trojnar <maciej.trojnar.ext@nokia.com>
2016-11-10 14:51:41 +0100

    implementation of find_if and inner_product

--- 40-algos-impl/find_if.h
+++ 40-algos-impl/find_if.h
@@ -1,8 +1,26 @@
 #pragma once
 
+//namespace training
+//{
+//template<typename InputIt, typename UnaryPredicate>
+//InputIt find_if(InputIt begin, InputIt end, UnaryPredicate pred);
+//}
+
+
+
 namespace training
 {
+
 template<typename InputIt, typename UnaryPredicate>
-InputIt find_if(InputIt begin, InputIt end, UnaryPredicate pred);
+InputIt find_if(InputIt begin, InputIt end, UnaryPredicate pred){
+    InputIt iter=begin;
+    for( ; iter != end; ++iter){
+        if (pred(*iter)){
+            break;
+        }
+    }
+    return iter;
+}
+
 }
 
