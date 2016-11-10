a6a39de3e94b64cb23ac77247ad30707b3d6ae54
Maciej Trojnar <maciej.trojnar.ext@nokia.com>
2016-11-10 12:45:56 +0100

    another naive refactor of implementation.h

--- 30-traits/implementation.h
+++ 30-traits/implementation.h
@@ -5,49 +5,11 @@
 
 
 template<typename T, typename T1, typename T2, typename T3>
-struct is_any_of : std::false_type
-{
-
-};
-
-template<typename T, typename T2, typename T3>
-struct is_any_of<T, T, T2, T3> : std::true_type
-{
-
-};
-
-template<typename T, typename T1, typename T3>
-struct is_any_of<T, T1, T, T3> : std::true_type
-{
-
-};
-
-template<typename T, typename T1, typename T2>
-struct is_any_of<T, T1, T2, T> : std::true_type
-{
-
-};
-
-template<typename T>
-struct is_any_of<T, T, T, T> : std::true_type
-{
-
-};
-
-template<typename T,  typename T3>
-struct is_any_of<T, T, T, T3> : std::true_type
-{
-
-};
-
-template<typename T, typename T2>
-struct is_any_of<T, T, T2, T> : std::true_type
-{
-
-};
-
-template<typename T, typename T1>
-struct is_any_of<T, T1, T, T> : std::true_type
+struct is_any_of : std::bool_constant<
+        std::is_same<T, T1>::value
+        || std::is_same<T, T2>::value
+        || std::is_same<T, T3>::value
+        >
 {
 
 };
